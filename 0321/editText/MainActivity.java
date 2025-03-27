6ae58268fcbf8c3aa908f4d4882eb9ce876d9ee0
yunji0915 <jackie9288@naver.com>
2025-03-27 14:35:01 +0900

    Add files via upload

---
+++ 0321/editText/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.edittext; // 프로젝트에서 설정한 패키지 이름
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = editText.getText().toString();
+                textView.setText(inputText);
+            }
+        });
+    }
+}
